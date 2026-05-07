a1a92256143b302eb964ef15734ca034f2d8fdea
fouad64 <fouadyasser20@gmail.com>
2026-05-07 21:37:19 +0300

    feat: initialize database schema with new DB.sql file

---
+++ DB.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Nodes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+
+    type VARCHAR(20) CHECK (type IN ('UPSTREAM', 'DOWNSTREAM')),
+
+    protocol VARCHAR(50), 
+    -- Example: FTP, SFTP, SCP (you can also normalize this later)
+
+    authentication VARCHAR(50),
+
+    username VARCHAR(100),
+    password VARCHAR(100),
+
+    ip_address VARCHAR(45),
+    port INT
+);
+=====================================================
+=====================================================
+CREATE TABLE Mediation_Rules (
+    id SERIAL PRIMARY KEY,
+
+    source_node_id INT NOT NULL,
+    destination_node_id INT NOT NULL,
+
+    FOREIGN KEY (source_node_id) REFERENCES Nodes(id) ON DELETE CASCADE,
+    FOREIGN KEY (destination_node_id) REFERENCES Nodes(id) ON DELETE CASCADE
+);
